8e1f1da21135bb0b890bfbd3303aec08e6d2714c
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-07-13 15:21:54 -0400

    Squashed commit of the following:
    commit 59dcd1c0df71a5fdb56a2c930590e2205dce5f68
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 15:20:57 2018 -0400
    
        AC
    
    commit 26df567b0dd87d1010f4db4f60e7cc533ab9a543
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 15:16:21 2018 -0400
    
        AC
    
    commit bd4a86e7b015c879df173587b01935c005be886e
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 15:14:52 2018 -0400
    
        AC
    
    commit 0e63d4c945febacdb98a07499f40e3e62cd97345
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 15:06:14 2018 -0400
    
        modify
    
    commit d1cdffd5b55b3c94a219087ee51d62a41961e882
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 14:57:20 2018 -0400
    
        AC
    
    commit 40f331472375a0468a8e896fd5cd0005669733ee
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 14:47:59 2018 -0400
    
        AC
    
    commit e048d7866788bc997fe51ba0b750d6c75d74e879
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 14:37:49 2018 -0400
    
        AC
    
    commit bbb3400de0726f36d3f210d7bb512887c526c2e4
    Author: Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
    Date:   Fri Jul 13 14:32:04 2018 -0400
    
        single

--- frontend.py
+++ frontend.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 
-from functools import partial
+import collections
+import functools
+import itertools
+from multiprocessing import Pool, cpu_count
+import os
+import sys
 import subprocess as sp
 import numpy as np
 
-def wrap_parallel():
-    from backend import Rewrite, exec_parallel
-    k = 2
-    params = [[1, 2, 3], [11.0, 12.0]]
-    fmts = ["%02d", "%.02f"]
-    changes = [[Rewrite("echo_numbers.sh", 2, "x", "x={p:02d}")],
-               [Rewrite("echo_numbers.sh", 3, "y", "y={p:f}")]]
-    res = exec_parallel("/home/tak/prgm/parallel_wrapper",
-                  k, params, fmts, changes, "sh echo_numbers.sh")
-    visualize_results(res)
+def main_parallel():
+    params = [
+        Param("first", "%02d", [1, 2, 3], [Rewriter("echo_numbers.sh", 2, "x", "x={param:02d}")]),
+        Param("second", "%.02f", [11.0, 12.0], [Rewriter("echo_numbers.sh", 3, "y", "y={param:f}")])
+    ]
+    res = Runner.run_parallel(
+        "/home/tak/prgm/parallel_wrapper", params, "sh echo_numbers.sh")
+    visualize_results(params, res)
 
-def visualize_results(res):
+def visualize_results(params, res):
     assert isinstance(res, np.ndarray)
+    print("\nvisualization:")
+    for ip, p in enumerate(params):
+        print(f"{ip}-th parameter '{p.name}' with length {len(p.values)}")
     print(res.shape)
     print(res)
 
-# for each realization, get_output_obj or get_output_obj is called from backend.py
 def get_output_obj():
     with open("out", "r") as f:
         lines = f.readlines()
@@ -29,6 +34,95 @@ def get_output_obj():
 def get_failed_obj():
     return "Failed"
 
+class Rewriter:
+    def __init__(self, relative_file_name, line_num, match, formattable_replacer):
+        assert isinstance(relative_file_name, str)
+        assert isinstance(line_num, int)
+        assert isinstance(match, str)
+        assert isinstance(formattable_replacer, str)
+        self.relative_file_name = relative_file_name
+        self.line_num = line_num
+        self.match = match
+        self.formattable_replacer = formattable_replacer
+
+    def rewrite_file_with_param(self, param):
+        with open(self.relative_file_name, "r") as f:
+            lines = f.readlines()
+        with open(self.relative_file_name, "w") as f:
+            for i, l in enumerate(lines):
+                if i == self.line_num - 1:
+                    assert self.match in l
+                    f.write(self.formattable_replacer.format(param=param) + "\n")
+                else:
+                    f.write(l)
+
+class Param:
+    def __init__(self, name, path_fmt, values, rewriters):
+        assert isinstance(values, collections.Iterable)
+        assert isinstance(rewriters, collections.Iterable)
+        self.name = name
+        self.path_fmt = path_fmt
+        self.values = values
+        self.rewriters = rewriters
+
+class Runner:
+    @classmethod
+    def inverse_itertools_kd_product(cls, param_vals, map_result):
+        k_dim = len(param_vals)
+        ns = [len(param_vals[j]) for j in range(k_dim)]
+        assert np.prod(ns, dtype=int) == len(map_result)
+        res_np = np.array(map_result, dtype=object)
+        res = res_np.reshape(ns)
+        return res
+
+    @classmethod
+    def shell(cls, cmd, writeout=False):
+        assert isinstance(cmd, str)
+        p = sp.run(cmd, shell=True, encoding="utf8", stderr=sp.PIPE, stdout=sp.PIPE)
+        if writeout:
+            with open("stdout", "w") as f:
+                f.write(p.stdout)
+            with open("stderr", "w") as f:
+                f.write(p.stdout)
+        else:
+            if p.returncode != 0:
+                print(p.stdout)
+                print(p.stderr)
+        if p.returncode != 0:
+            raise Exception("shell %s failed" % cmd)
+
+    @classmethod
+    def exec_single_job(cls, wdir_base, params, command, list_param_val):
+        k_dim = len(params)
+        assert len(list_param_val) == k_dim
+        str_path_part = [(params[j].path_fmt % list_param_val[j]).replace(".", "_") for j in range(k_dim)]
+        suffix_path = "".join([f"/{str_path_part[j]}" for j in range(k_dim)])
+        single_dir_name = f"{wdir_base}/wk_parallel{suffix_path}"
+        cls.shell(f"mkdir -p {single_dir_name}")
+        os.chdir(single_dir_name)
+        cls.shell(f"cp -rf {wdir_base}/template_parallel/* .")
+        for j in range(k_dim):
+            for r in params[j].rewriters:
+                r.rewrite_file_with_param(list_param_val[j])
+        try:
+            cls.shell(command, writeout=True)
+            res = get_output_obj()
+            print(f"util_parallel: {suffix_path} done")
+        except:
+            res = get_failed_obj()
+            print(f"util_parallel: {suffix_path} failed")
+        return res
+
+    @classmethod
+    def run_parallel(cls, wdir_base, params, command, max_proc=10):
+        cls.shell(f"rm -rf {wdir_base}/wk_parallel")
+        param_vals = [p.values for p in params]
+        param_vals_prod = itertools.product(*param_vals)
+        job = functools.partial(cls.exec_single_job, wdir_base, params, command)
+        with Pool(min(cpu_count(), max_proc)) as p:
+            res = p.map(job, param_vals_prod)
+        return cls.inverse_itertools_kd_product(param_vals, res)
+
 if __name__ == "__main__":
-    wrap_parallel()
+    main_parallel()
 
