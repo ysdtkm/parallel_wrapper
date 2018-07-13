1d657f92a9854ef2e8c81047d90f23d271482ab5
Takuma Yoshida <35924685+ysdtkm@users.noreply.github.com>
2018-07-13 13:58:08 -0400

    modify

--- frontend.py
+++ frontend.py
@@ -6,14 +6,13 @@ import numpy as np
 
 def wrap_parallel():
     from backend import Rewrite, exec_parallel
-    k = 3
-    params = [[1, 2, 3], [11.0, 12.0, 13.0], [5, 6]]
-    fmts = ["%02d", "%.02f", "%02d"]
+    k = 2
+    params = [[1, 2, 3], [11.0, 12.0]]
+    fmts = ["%02d", "%.02f"]
     changes = [[Rewrite("echo_numbers.sh", 2, "x", "x={p:02d}")],
-               [Rewrite("echo_numbers.sh", 3, "y", "y={p:f}")],
-               []]
+               [Rewrite("echo_numbers.sh", 3, "y", "y={p:f}")]]
     res = exec_parallel("/home/tak/prgm/parallel_wrapper",
-                  k, params[:k], fmts[:k], changes[:k], "sh echo_numbers.sh")
+                  k, params, fmts, changes, "sh echo_numbers.sh")
     visualize_results(res)
 
 def visualize_results(res):
